60e3b1f338cf268f826cf9b2f2a770eb60bd0ac0
Gilberto Junior <junior.siilva182@gmail.com>
2021-03-26 15:05:47 -0300

    styling inicial da tela photos

--- Estagio/src/screens/photos.screen.tsx
+++ Estagio/src/screens/photos.screen.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {SafeAreaView, StatusBar} from 'react-native';
+import styled from 'styled-components/native';
+import PhotoList from '../components/photoList.component';
+
+const SafeArea = styled(SafeAreaView)`
+  flex: 1;
+  ${StatusBar.currentHeight && `margin-top: ${StatusBar.currentHeight}px`};
+`;
+
+const Container = styled.View`
+  flex: 1;
+  background-color: #fff;
+`;
 
 export const Photos = () => {
   return (
-    <View>
-      <Text>Photos show</Text>
-    </View>
+    <SafeArea>
+      <Container>
+        <PhotoList />
+      </Container>
+    </SafeArea>
   );
 };
