1fea8960a59c9e6d78e640f66c31dc9b18ba72bb
Gilberto Junior <junior.siilva182@gmail.com>
2021-04-02 15:35:47 -0300

    adicionado a action SET_QUERY

--- Estagio/src/redux/reducers/photoReducer.ts
+++ Estagio/src/redux/reducers/photoReducer.ts
@@ -1,18 +1,31 @@
-import {GET_PHOTOS, SET_LOADING, PHOTOS_ERROR} from '../actions/types';
+import {
+  GET_PHOTOS,
+  SET_LOADING,
+  PHOTOS_ERROR,
+  SET_QUERY,
+} from '../actions/types';
 
 const initialState = {
   photos: null,
   loading: false,
   error: null,
+  query: null,
 };
 
 export default (state = initialState, action: {type: any; payload: any}) => {
   switch (action.type) {
     case GET_PHOTOS:
       return {
+        ...state,
         type: GET_PHOTOS,
         photos: action.payload,
         loading: false,
+      };
+    case SET_QUERY:
+      return {
+        ...state,
+        type: SET_QUERY,
+        query: action.payload,
       };
     case SET_LOADING:
       return {
